a836198a147fe141825c6510bd8a7dad97405fb1
andrey231632 <zolin.andrey333@gmail.com>
2024-02-26 11:12:15 +0500

    update

--- api/api_services/schemes.py
+++ api/api_services/schemes.py
@@ -1,37 +1,3 @@
-<<<<<<< HEAD
-from typing import Optional
-from pydantic import BaseModel, Field, condecimal
-from datetime import datetime, date
-
-
-class UsersDataBase(BaseModel):
-    login: str = Field(..., description='user\'s login data')
-    password: str = Field(..., description='Users\' password')
-    role: int = Field(..., description='user\'s role')
-
-class TopicsDataBase(BaseModel):
-    user_id: str = Field(..., description='user id')
-    title: str = Field(..., description='title of the topic')
-    date: str = Field(..., description='date of the topic')
-
-class MessagesDataBase(BaseModel):
-    topic_id: int = Field(..., description='id of the topic')
-    content: str = Field(..., description='main text message')
-
-class LoginRequestDTO(BaseModel):
-    login: str = Field(..., description='user\'s username or email')
-    password: str = Field(..., description='user\'s password without hash')
-
-class SingupRequestDTO(BaseModel):
-    login: str = Field(..., description='user\'s username or email')
-    password: str = Field(..., description='user\'s password without hash')
-    teacher_student_flag: int = Field(..., description='teacher or student flag')
-
-class LoginResponseDTO(BaseModel):
-    response: bool = Field(..., description='give access or not')
-
-class SingupResponseDTO(BaseModel):
-=======
 from typing import Optional
 from pydantic import BaseModel, Field, condecimal
 from datetime import datetime, date
@@ -67,5 +33,4 @@ class LoginResponseDTO(BaseModel):
     response: bool = Field(..., description='give access or not')
 
 class SingupResponseDTO(BaseModel):
->>>>>>> ecd7761f473415b6450b959cf902accbfd58077b
     response: bool = Field(..., description='give access or not')
--- api/database_handler.py
+++ api/database_handler.py
@@ -1,68 +1,3 @@
-<<<<<<< HEAD
-from .utils.singleton import Singleton
-from .utils.config import DATABASE_PATH
-import sqlite3
-
-
-class DataBase(Singleton):
-    
-    def init(self):
-        super().__init__()
-        self.db_path = DATABASE_PATH
-
-    # users
-    def addUser(self, login, hash_password: str, role: int):
-        with sqlite3.connect(self.db_path) as c:
-            c.execute('INSERT INTO users (login, hash_password, role) VALUES (?, ?, ?, ?)', (login, hash_password, role))
-            c.commit()
-            print(f'[INFO] new user "{login}" has been added')
-    
-    def getUser(self, login: str):
-        with sqlite3.connect(self.db_path) as c:
-            user_info = c.execute('SELECT * FROM users WHERE login = ?', (login,)).fetchone()
-            return user_info
-
-    def getPasswordFromLogin(self, login: str):
-        with sqlite3.connect(self.db_path) as c:
-            hash_password = c.execute('SELECT hash_password FROM users WHERE login = ?', (login,)).fetchone()
-            return hash_password[0]
-
-    def getAllUsersLogin(self):
-        with sqlite3.connect(self.db_path) as c:
-            logins = c.execute('SELECT login FROM users').fetchall()
-            return logins
-        
-    def getAllPasswords(self):
-        with sqlite3.connect(self.db_path) as c:
-            hash_passwords = c.execute('SELECT hash_password FROM users').fetchall()
-            return hash_passwords
-
-    
-
-    # Assistant
-    def addTopic(self, user_id: int, title: str, date: str): 
-        with sqlite3.connect(self.db_path) as c:
-            c.execute('INSERT INTO topics (user_id, title, date) VALUES (?, ?, ?)', (user_id, title, date))
-            c.commit()
-        
-    def getTopicsFromUserId(self, user_id: str): 
-        with sqlite3.connect(self.db_path) as c:
-            topics = c.execute('SELECT * FROM topics WHERE user_id = ?', (user_id,)).fetchall()
-            return topics
-
-    def addMessage(self, topic_id: int, content: str): 
-        with sqlite3.connect(self.db_path) as c:
-            c.execute('INSERT INTO messages (topic_id, content) VALUES (?, ?)', (topic_id, content))
-            c.commit()
-
-    def getMessagesFromTopicId(self, topic_id: int): 
-        with sqlite3.connect(self.db_path) as c:
-            c.execute('SELECT * FROM messages WHERE topic_id = ?' (topic_id,)).fetchall()
-            c.commit()
-
-
-
-=======
 from .utils.singleton import Singleton
 from .utils.config import DATABASE_PATH
 import sqlite3
@@ -125,4 +60,3 @@ class DataBase(Singleton):
             return messages
 
 
->>>>>>> ecd7761f473415b6450b959cf902accbfd58077b
