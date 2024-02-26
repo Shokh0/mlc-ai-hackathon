dfa4c533f606e1ab6a4a3c666eb84796329fde52
andrey231632 <zolin.andrey333@gmail.com>
2024-02-26 09:41:01 +0500

    add "front" folder

--- api/utils/config.py
+++
@@ -1,1 +0,0 @@
-DATABASE_PATH = "api/db.sqlite3"
--- api/utils/usefull_func.py
+++
@@ -1,8 +0,0 @@
-import hashlib
-
-def hash_password(password):
-    password_bytes = password.encode('utf-8')
-    hash_object = hashlib.sha256()
-    hash_object.update(password_bytes)
-    hashed_password = hash_object.hexdigest()
-    return hashed_password
